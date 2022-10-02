849457eb6daae9bc0bcb2e295900859d3b836447
sanket381 <salunkesanket381@gmail.com>
2022-10-02 22:07:48 +0530

    Updates

---
+++ precache-manifest.5d01a4ae41d7dbe4e01c96756e0c49e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b7ddca7eb9f8b5a4100",
+    "url": "/keeperAPP/static/js/main.5b7ddca7.chunk.js"
+  },
+  {
+    "revision": "61a0396228d0f56fffe4",
+    "url": "/keeperAPP/static/js/1.61a03962.chunk.js"
+  },
+  {
+    "revision": "e08daf21f2f7a282331c",
+    "url": "/keeperAPP/static/js/runtime~main.e08daf21.js"
+  },
+  {
+    "revision": "b200d43335fabd2a05408da44fcf9453",
+    "url": "/keeperAPP/index.html"
+  }
+];
